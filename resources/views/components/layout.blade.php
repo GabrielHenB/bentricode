df6c7f76638537fd6e5fda18c8d297860dd5961e
Gabriel <ghbraz17@gmail.com>
2023-08-05 11:21:27 -0300

    Indentacoes e outras coisas

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,42 +17,42 @@
     <!-- Header inicio -->
     <!-- Isso depois pode ser extraido para um Blade component -->
     <header class="container header-conteiner">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Bentri<span style="color:#782FB7;">Code</span></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('posts.index')}}">Notícias</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-        </ul>
-             <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Bentri<span style="color:#782FB7;">Code</span></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('posts.index')}}">Posts</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Visitante
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Carrinho</a></li>
+                <li><a class="dropdown-item" href="#">Informações</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" style='color:darkred;' href="#">Sair</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Admin</a>
+            </li>
+            </ul>
+            <form class="d-flex" method="GET" action="?squery={{strip_tags(request('squery'))}}">
+                <input class="form-control me-2" type="search" name="squery" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Pesquisar</button>
             </form>
-            </div>
+          </div>
         </div>
-    </nav>
+      </nav>
     </header>
     <!-- Header final -->
     <!-- Conteudo inicio -->
