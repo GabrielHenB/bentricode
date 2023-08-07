a60c2d4843d3495f65f67808c87f59243a0a2db3
marlon-bento <marlon_lion@hotmail.com>
2023-08-07 14:43:00 -0300

    alteração de background e carousel além de correções de bugs

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,9 +1,8 @@
 @props(['item'])
 
-            <div class="card-body">
-                <h5 class="card-title">{{$item->title}}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Publicado a {{$item->created_at}}</h6>
-                <p class="card-text">{{ $item->body }}</p>
-                <a href="#" class="card-link">ler mais</a>
-            </div>
-  
+<div class="card-body">
+    <h5 class="card-title">{{$item->title}}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Publicado a {{$item->created_at}}</h6>
+    <p class="card-text">{{ $item->body }}</p>
+    <a href="#" class="card-link">ler mais</a>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,60 @@
 @extends('components.layout')
 {{-- SECAO DO TITULO DA PAGINA ISSO APARECE NO NAVEGADOR --}}
 @section('adicional')
-    - Home
-@endsection  
+- Home
 
+@endsection
+@section('carrosel')
+<x-carrousel />
+@endsection
 {{-- SECAO DO CONTEUDO --}}
 
 @section('content')
-    <div class='container'>
-        <div class='row'>
-            <h2 class="col-12">Bentricode: Inovação e Excelência em Soluções de Software</h2>
-            <p class="col-12">
+<div class='container'>
+    
+    <div class='row'>
+        <h2 class="col-12">Bentricode: Inovação e Excelência em Soluções de Software</h2>
+        <p class="col-12">
 
-                Bem-vindo ao mundo da Bentricode, uma empresa de software
-                líder em inovação e excelência na criação de soluções t
-                ecnológicas sob medida. Fundada em 2019 por uma dupla de
-                visionários apaixonados por tecnologia, a Bentricode
-                rapidamente se tornou uma referência no setor de desenvolvimento de software.
-                <br>
-                Nossa equipe é composta por profissionais altamente qual
-                ificados e apaixonados por transformar ideias em realida
-                de digital. Com décadas de experiência combinada, somos
-                especializados em diversas áreas, incluindo desenvolvim
-                ento de aplicativos móveis, softwares empresariais, sis
-                temas de gestão, soluções de e-commerce, inteligência a
-                rtificial e muito mais.
-                <br>
-                Na Bentricode, a satisfação do cliente é nossa prioridad
-                e número um. Estamos comprometidos em entender as necessi
-                dades específicas de cada cliente e entregar soluções que
-                impulsionem seus negócios de forma eficiente e inovadora.
-                Nossa abordagem personalizada garante que cada projeto 
-                seja tratado com a devida atenção e cuidado, independent
-                emente do tamanho ou complexidade.
-                <br>
-                Além disso, a Bentricode se orgulha de estar na vanguar
-                da da tecnologia, acompanhando as últimas tendências do m
-                ercado e incorporando as melhores práticas em nossos proj
-                etos. Estamos sempre em busca de aprimorar nossas habilida
-                des e conhecimentos para oferecer aos nossos clientes solu
-                ções verdadeiramente vanguardistas.
-                <br>
-                Confie na Bentricode para impulsionar a sua empresa rumo
-                ao sucesso tecnológico. Estamos prontos para enfrentar o
-                s desafios mais ambiciosos e transformar suas ideias em 
-                realidade. Contate-nos hoje mesmo e descubra como a parce
-                ria com a Bentricode pode elevar seus negócios a um novo patamar tecnológico.</p>
-            
+            Bem-vindo ao mundo da Bentricode, uma empresa de software
+            líder em inovação e excelência na criação de soluções t
+            ecnológicas sob medida. Fundada em 2019 por uma dupla de
+            visionários apaixonados por tecnologia, a Bentricode
+            rapidamente se tornou uma referência no setor de desenvolvimento de software.
+            <br>
+            Nossa equipe é composta por profissionais altamente qual
+            ificados e apaixonados por transformar ideias em realida
+            de digital. Com décadas de experiência combinada, somos
+            especializados em diversas áreas, incluindo desenvolvim
+            ento de aplicativos móveis, softwares empresariais, sis
+            temas de gestão, soluções de e-commerce, inteligência a
+            rtificial e muito mais.
+            <br>
+            Na Bentricode, a satisfação do cliente é nossa prioridad
+            e número um. Estamos comprometidos em entender as necessi
+            dades específicas de cada cliente e entregar soluções que
+            impulsionem seus negócios de forma eficiente e inovadora.
+            Nossa abordagem personalizada garante que cada projeto
+            seja tratado com a devida atenção e cuidado, independent
+            emente do tamanho ou complexidade.
+            <br>
+            Além disso, a Bentricode se orgulha de estar na vanguar
+            da da tecnologia, acompanhando as últimas tendências do m
+            ercado e incorporando as melhores práticas em nossos proj
+            etos. Estamos sempre em busca de aprimorar nossas habilida
+            des e conhecimentos para oferecer aos nossos clientes solu
+            ções verdadeiramente vanguardistas.
+            <br>
+            Confie na Bentricode para impulsionar a sua empresa rumo
+            ao sucesso tecnológico. Estamos prontos para enfrentar o
+            s desafios mais ambiciosos e transformar suas ideias em
+            realidade. Contate-nos hoje mesmo e descubra como a parce
+            ria com a Bentricode pode elevar seus negócios a um novo patamar tecnológico.
+        </p>
 
-        </div>
+
     </div>
+</div>
 @endsection
 
 {{-- SECAO DA BARRA LATERAL DIREITA --}}
